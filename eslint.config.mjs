d7e3a862938828d9c91277cd02fa4c0f1670361d
panovark <arkadiypanov.dev@gmail.com>
2025-11-03 17:24:18 +0100

    chore: bootstrap react app with tooling
    - init Vite React app structure
    
    - add ESLint/Prettier configs and scripts
    
    - include React Query and chart dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  ...pluginQuery.configs["flat/recommended"],
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+  prettier,
+];
