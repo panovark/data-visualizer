52fa854c275000927c179f7f0b84d17fdb6bc210
panovark <arkadiypanov.dev@gmail.com>
2025-11-05 23:29:00 +0100

    feat: wrap app component in strictmode and dataloader in error boundary to improve stability

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,26 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import DataLoader from "@/components/DataLoader";
 import ThemeProvider from "@/components/ThemeProvider";
+import ErrorBoundary from "@/components/ErrorBoundary";
 import "@/index.css";
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <DataLoader />
-      </ThemeProvider>
-      {import.meta.env.DEV && <ReactQueryDevtools />}
-    </QueryClientProvider>
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <ErrorBoundary>
+            <DataLoader />
+          </ErrorBoundary>
+        </ThemeProvider>
+        {import.meta.env.DEV && <ReactQueryDevtools />}
+      </QueryClientProvider>
+    </StrictMode>
   );
 };
 
