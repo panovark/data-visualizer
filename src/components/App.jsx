64efe2b7847df45eaec8ec49c9d539a35e293dc4
panovark <arkadiypanov.dev@gmail.com>
2025-11-27 15:27:59 +0100

    fix: wrap app component in errorboundary for improved error handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import DataLoader from "@/components/DataLoader";
 import ThemeProvider from "@/components/ThemeProvider";
-import ErrorBoundary from "@/components/ErrorBoundary";
 
 const queryClient = new QueryClient();
 
@@ -10,9 +9,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <ErrorBoundary>
-          <DataLoader />
-        </ErrorBoundary>
+        <DataLoader />
       </ThemeProvider>
       {import.meta.env.DEV && <ReactQueryDevtools />}
     </QueryClientProvider>
